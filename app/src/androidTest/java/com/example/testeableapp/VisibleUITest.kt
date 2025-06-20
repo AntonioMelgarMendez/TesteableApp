3c20b95a6c141dfda6e8a9bf4519e6329858450a
WalterRB28 <00018523@uca.edu.sv>
2025-06-20 15:15:14 -0600

    adding visible UI upgrade

--- app/src/androidTest/java/com/example/testeableapp/VisibleUITest.kt
+++ app/src/androidTest/java/com/example/testeableapp/VisibleUITest.kt
@@ -3,6 +3,7 @@ package com.example.testeableapp
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -16,9 +17,22 @@ class VisibleUITest {
   @Test
   fun test_ui_elements_are_visible() {
     composeTestRule.onNodeWithText("Calculadora de Propinas").assertIsDisplayed()
+
     composeTestRule.onNodeWithText("Monto de la cuenta").assertIsDisplayed()
+
     composeTestRule.onNodeWithText("Porcentaje de propina: 15%").assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("tipSlider").assertIsDisplayed()
+
     composeTestRule.onNodeWithText("Número de personas: 1").assertIsDisplayed()
+    composeTestRule.onNodeWithText("-").assertIsDisplayed()
+    composeTestRule.onNodeWithText("+").assertIsDisplayed()
+    composeTestRule.onNodeWithText("1").assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("roundTipCheckbox").assertIsDisplayed()
     composeTestRule.onNodeWithText("Redondear propina").assertIsDisplayed()
+
+    composeTestRule.onNodeWithText("Propina: $0.00").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Total por persona: $0.00").assertIsDisplayed()
   }
 }
